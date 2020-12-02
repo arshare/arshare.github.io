67eef1180f3d235639e3db57c0a0654077cc9ec7
ar@ <ar@>
2020-12-02 13:15:34 -0600

    Deployed  with MkDocs version: 1.1.2

--- generator.js
+++ generator.js
@@ -18,6 +18,18 @@ let zdata = `\
 
 `;
 
+// Fns padding related; pad Fn normally pads to 3 length; pass in 2 for padLength if wanted.
+var highestLessonNumber = jsonData.lessons.length - 1;
+var padLength = highestLessonNumber >= 100 ? 3 : highestLessonNumber >= 10 ? 2 : 1;
+function pad(number, padLength){
+    number = number && (number+'');
+    var paddednumber = !number ? null : number.length==3 ? number : number.length==2? ('0'+number) : number.length==1? ('00'+number) : null;
+    if(padLength == 2)
+        paddednumber = !number ? null : number.length==2 ? number : number.length==1? ('0'+number) : null;
+    return paddednumber;
+}
+
+
 jsonData.lessons.forEach((x, index) => { 
     var temp,
         lesson = jsonData.lessons[index],
@@ -25,7 +37,8 @@ jsonData.lessons.forEach((x, index) => {
         yt = lesson.yt, // ex: https://www.youtube.com/watch?v=piwJQkD47Y0&list=PLzn0qdi6JpdvvXVuJ7kIusNquSxeyKJvc
         ytCode = yt.replace('https://www.youtube.com/watch?v=', '').replace('&list=PLzn0qdi6JpdvvXVuJ7kIusNquSxeyKJvc', ''),
         ytEmbed = 'https://www.youtube-nocookie.com/embed/' + ytCode + '?start=0',
-        filename = (index+1) + '.md', // TODO: later probably add padding to number??
+        filenameNoExt = (index+1), // TODO: later probably add padding to number??
+        filename = pad(filenameNoExt, padLength) + '.md',
         zzz;
     temp = zdata;
     temp = temp.replace(/\{TITLE\}/g, title);
