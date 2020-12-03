d94d0302ce9c9a3a9f9d4ffcf8e54d70c79d0825
ar@ <ar@>
2020-12-03 09:10:07 -0600

    misc

--- docs_generator/generator.js
+++ docs_generator/generator.js
@@ -3,19 +3,39 @@
 const fs = require('fs');
 let balagha = require('./balagha.json');
 let nahw = require('./nahw.json');
+let sarf = require('./sarf.json');
 
 let jsonData = {
     // balagha,
-    nahw,
+    // nahw,
+    sarf,
 };
- 
+
+// Support multiple collections of data
+var collections = [
+    // 'balagha',
+    // 'nahw',
+    'sarf',
+];
+
 /// let data = JSON.stringify(jsonData, null, 2);
 
+let zdata_yt_title = '# {TITLE}\n\n';
+let zdata_yt_iframe = `\
+<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/{VID}?start=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe><BR>\n\n\
+`;
+let zdata_resources = '\n\n## Resources:\n';
+let zdata_slide = '- [Slides](https://github.com/arshare/resources_balagha_pdfs)\n';
+let zdata_yt_link = '- [Youtube video]({YT})\n';
+
+function stitchData(){
+
+}
 let zdata = `\
 # {TITLE}
-                
-<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/{VID}?start=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen">
-</iframe><BR>
+
+<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/{VID}?start=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
+<BR>
 
 ## Resources:
 - [Slides](https://github.com/arshare/resources_balagha_pdfs)
@@ -37,11 +57,6 @@ function pad(number, padLength){
     return paddednumber;
 }
 
-// Support multiple collections of data
-var collections = [
-    // 'balagha',
-    'nahw',
-];
 
 collections.forEach((collection, n) => {
 
@@ -49,21 +64,32 @@ collections.forEach((collection, n) => {
         var temp,
             lesson = jsonData[ collection ].lessons[index],
             title = lesson.title,
-            yt = !lesson.yt ? null : ( !Array.isArray(lesson.yt) ? lesson.yt : lesson.yt[ 0 ] ), // ex: https://www.youtube.com/watch?v=piwJQkD47Y0&list=PLzn0qdi6JpdvvXVuJ7kIusNquSxeyKJvc
-            ytCode = yt && yt.replace('https://www.youtube.com/watch?v=', '')
-                             .replace('&list=PLzn0qdi6JpdvvXVuJ7kIusNquSxeyKJvc', '')
-                             .replace('&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD', '')
-                             .replace(/\&index=[0-9]+/, ''),
-            ytEmbed = ytCode && ('https://www.youtube-nocookie.com/embed/' + ytCode + '?start=0'),
+            yts = !lesson.yt ? [] : ( !Array.isArray(lesson.yt) ? [ lesson.yt ] : lesson.yt ), 
+            ytCodes = [],
             filenameNoExt = (index+1), // TODO: later probably add padding to number??
             padLength = getPadLength( jsonData[ collection ] ),
-            filename = './'+ collection +'/'+ pad(filenameNoExt, padLength) + '.md',
+            filename = '../docs/'+ collection +'/'+ pad(filenameNoExt, padLength) + '.md',
             zzz;
-        temp = zdata;
-        temp = temp.replace(/\{TITLE\}/g, title);
-        temp = temp.replace(/\{VID\}/g, ytCode);
-        temp = temp.replace(/\{YT\}/g, yt);
-        // console.log([x, index, title, yt, ytCode])
+        temp = zdata_yt_title.replace(/\{TITLE\}/g, title);
+        yts.forEach((yt) => { if(!yt){ ytCodes.push(null); return; }
+            // yt ex: https://www.youtube.com/watch?v=piwJQkD47Y0&list=PLzn0qdi6JpdvvXVuJ7kIusNquSxeyKJvc
+            var ytCode = yt && yt.replace('https://www.youtube.com/watch?v=', '')
+                             .replace('&list=PLzn0qdi6JpdvvXVuJ7kIusNquSxeyKJvc', '') // balagha yt playlist
+                             .replace('&list=PLzn0qdi6JpdtdAyaM2yvvY1Yk9i4EpLHD', '') // nahw yt playlist
+                             .replace('&list=PLzn0qdi6JpdvWf0IDGNfaiM-okPqDuQoc', '') // sarf yt playlist
+                             .replace('&index=$INDEX', '')
+                             .replace(/\&index=[0-9]+/, '');
+                // ytEmbed = ytCode && ('https://www.youtube-nocookie.com/embed/' + ytCode + '?start=0');
+            ytCodes.push( ytCode );
+        });
+        ytCodes.forEach((ytCode) => {
+            temp += zdata_yt_iframe.replace(/\{VID\}/g, ytCode);
+        });
+        temp += zdata_resources;
+        temp += zdata_slide;
+        yts.forEach((yt) => {
+            temp += zdata_yt_link.replace(/\{YT\}/g, yt);
+        });
 
         fs.writeFile(filename, temp, (err) => {
             if (err) throw err;
